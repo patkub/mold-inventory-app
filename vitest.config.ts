dba28dbabef65b794ecc611656a96b5ac695122a
Patrick Kubiak <epicpatka@gmail.com>
2025-07-25 18:17:13 -0400

    merge unit tests for cloudflare worker and vitest components

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,32 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig, defineProject } from "vitest/config";
+import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 import tsconfigPaths from 'vite-tsconfig-paths'
- 
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
   test: {
-    environment: 'jsdom',
-    include: ['components/*.test.tsx'],
-    setupFiles: 'vitest-setup.js',
+    // "test.workspace" is now "test.projects"
+    projects: [
+      defineWorkersProject({
+        test: {
+          name: "Workers",
+          include: ["**/*worker.test.ts"],
+          poolOptions: {
+            workers: {
+              wrangler: { configPath: "./wrangler.jsonc" },
+            },
+          },
+        },
+      }),
+      defineProject({
+        plugins: [tsconfigPaths(), react()],
+        test: {
+          name: "Components",
+          environment: 'jsdom',
+          include: ['components/*.test.tsx'],
+          setupFiles: 'vitest-setup.js',
+        },
+      })
+    ],
   },
-})
+});
